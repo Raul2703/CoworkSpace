3b21c4a8f0832973d94617e7c864ae47e828a71b
Raul2703 <ramirez.dorau25@triana.salesianos.edu>
2026-05-05 10:22:11 +0200

    Primer commitç

---
+++ src/test/java/com/salesianostriana/dam/coworkspace/CoworkSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.coworkspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoworkSpaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
