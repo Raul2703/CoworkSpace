7efb07304ad6db809476ed11f022ef41f80e7758
Raul2703 <ramirez.dorau25@triana.salesianos.edu>
2026-05-13 14:27:09 +0200

    feat: calcular precio automatico de reservas

--- src/main/java/com/salesianostriana/dam/coworkspace/service/ReservaService.java
+++ src/main/java/com/salesianostriana/dam/coworkspace/service/ReservaService.java
@@ -9,4 +9,22 @@ import com.salesianostriana.dam.coworkspace.service.base.BaseServiceImpl;
 @Service
 public class ReservaService extends BaseServiceImpl<Reserva, Long, ReservaRepository> {
 
+	@Override
+	public Reserva save(Reserva reserva) {
+
+		calcularPrecioTotal(reserva);
+
+		return super.save(reserva);
+	}
+
+	private void calcularPrecioTotal(Reserva reserva) {
+
+		int horaInicio = Integer.parseInt(reserva.getHoraInicio().substring(0, 2));
+		int horaFin = Integer.parseInt(reserva.getHoraFin().substring(0, 2));
+
+		int horasReservadas = horaFin - horaInicio;
+
+		reserva.setPrecioTotal(horasReservadas * 150.0);
+	}
+
 }
