34865c5360795023fd2032841e5567aa0151e879
Raul2703 <ramirez.dorau25@triana.salesianos.edu>
2026-05-15 12:59:09 +0200

    feat: configurar seguridad y login

--- src/main/java/com/salesianostriana/dam/coworkspace/config/SecurityConfig.java
+++ src/main/java/com/salesianostriana/dam/coworkspace/config/SecurityConfig.java
@@ -3,30 +3,43 @@ package com.salesianostriana.dam.coworkspace.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-		http.authorizeHttpRequests(auth -> auth.requestMatchers("/", "/login").permitAll()
-				.requestMatchers("/admin/**", "/usuarios/**", "/espacios/**", "/reservas/**").authenticated()
-				.anyRequest().permitAll())
+		http.authorizeHttpRequests(auth -> auth.requestMatchers("/", "/index", "/login", "/css/**", "/js/**", "/img/**")
+				.permitAll().requestMatchers("/admin/**", "/usuarios/**", "/espacios/**", "/reservas/**")
+				.authenticated().anyRequest().authenticated())
+
+				.requestCache(cache -> {
+					HttpSessionRequestCache requestCache = new HttpSessionRequestCache();
+					requestCache.setMatchingRequestParameterName(null);
+					cache.requestCache(requestCache);
+				})
+
 				.formLogin(login -> login.loginPage("/login").defaultSuccessUrl("/admin", true).permitAll())
-				.logout(logout -> logout.logoutSuccessUrl("/").permitAll());
+
+				.logout(logout -> logout.logoutUrl("/logout").logoutSuccessUrl("/").invalidateHttpSession(true)
+						.deleteCookies("JSESSIONID").permitAll());
 
 		return http.build();
 	}
 
 	@Bean
-	UserDetailsService userDetailsService() {
+	UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
 
-		var admin = User.withDefaultPasswordEncoder().username("admin").password("admin").roles("ADMIN").build();
+		var admin = User.builder().username("admin").password(passwordEncoder.encode("admin")).roles("ADMIN").build();
 
 		return new InMemoryUserDetailsManager(admin);
 	}
